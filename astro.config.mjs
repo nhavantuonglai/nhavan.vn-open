478cb2d8887e5ad3a3705014ac6defd02baf828c
nhavantuonglai <info@nhavantuonglai.com>
2025-08-15 20:37:24 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,56 +25,45 @@ const whenExternalScripts = (items = []) =>
 		: [];
 
 export default defineConfig({
+
 	output: 'static',
-	site: 'https://nhavantuonglai.com',
-	build: {
-		assetsPrefix: '/assets/', // Gom các tệp tĩnh vào thư mục /assets/
-		inlineStylesheets: 'always', // Nhúng CSS trực tiếp vào HTML
-	},
+
+	site: 'https://nhavan.vn',
+
 	integrations: [
-		react({
-			experimentalReactChildren: true, // Tối ưu hóa React để giảm JS riêng biệt
-			include: ['**/components/*.jsx'], // Chỉ xử lý các thành phần React cần thiết
-		}),
+
+		react(),
 		tailwind({
 			applyBaseStyles: false,
 		}),
+
 		sitemap(),
+
 		mdx(),
+
 		...whenExternalScripts(() =>
 			partytown({
 				config: { forward: ['dataLayer.push'] },
 			})
 		),
+
 		astrowind(),
 	],
+
 	markdown: {
 		remarkPlugins: [readingTimeRemarkPlugin],
 		rehypePlugins: [responsiveTablesRehypePlugin, lazyImagesRehypePlugin],
 	},
+
 	vite: {
-		optimizeDeps: {
-			exclude: ['_astro/*'], // Ngăn Vite tạo tệp JS riêng trong _astro
-		},
-		build: {
-			rollupOptions: {
-				output: {
-					manualChunks: {
-						'client': ['src/client', 'src/components'], // Gộp JS từ client và components
-						'vendor': ['src/integration'], // Gộp JS từ astrowind
-					},
-					// Đặt tên tệp JS để dễ quản lý
-					chunkFileNames: 'assets/js/[name].js',
-					assetFileNames: 'assets/[ext]/[name].[ext]',
-				},
-			},
-			minify: 'esbuild', // Tối ưu hóa và gộp JS
-		},
+
 		resolve: {
 			alias: {
 				'~': path.resolve(__dirname, './src'),
 			},
-			extensions: ['.js', '.ts'],
+			extensions: ['.js', '.ts']
 		},
+
 	},
+
 });
